662a356bfeb49f70b879de7ec076a56ba9b5f73d
Róbert Viðar Bjarnason <robert@idea-synergy.com>
2013-08-19 20:18:41 +0000

    Fixes

--- app/models/trading_operation.rb
+++ app/models/trading_operation.rb
@@ -52,7 +52,7 @@ class TradingOperation
                                                   :description=>"Trading Time Frame Start"})
     events << simulated_trading_signal_to_amchart({:name=>"E", :current_date_time=>DateTime.parse("#{@day} #{@to_hour}:00:00"), :background_color=>"#ff6655",
                                                   :description=>"Trading Time Frame Stop"})
-    trading_signals.where(:created_at.gtl => from_date, :created_at.lte=>to_date).all.each do |signal|
+    trading_signals.where(:created_at.gte => from_date, :created_at.lte=>to_date).all.each do |signal|
       if signal.name=="Short Open" or signal.name=="Long Open"
 #        events << simulated_trading_signal_to_amchart({:name=>"#{signal.name}",
 #                                                       :type=>"arrowUp",
